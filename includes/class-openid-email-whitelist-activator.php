9efd43d8e79a97c827456e87e82f51867c38f4e9
Joachim Happel <joachim.happel@gmail.com>
2024-12-01 12:44:13 +0100

    Create class-openid-email-whitelist-activator.php

---
+++ includes/class-openid-email-whitelist-activator.php
@@ -0,0 +1,75 @@
+<?php
+namespace OpenIDWhitelist;
+
+class Activator {
+    public static function activate() {
+        if (!is_plugin_active('openid-connect-generic/openid-connect-generic.php')) {
+            deactivate_plugins(plugin_basename(__FILE__));
+            wp_die(
+                __('OpenID Connect Email Whitelist benötigt das Plugin "OpenID Connect Generic". Bitte installieren und aktivieren Sie es zuerst.', 'openid-connect-whitelist'),
+                __('Plugin Aktivierungsfehler', 'openid-connect-whitelist'),
+                ['back_link' => true]
+            );
+        }
+
+        // Standardeinstellungen für Network
+        if (is_multisite()) {
+            $network_defaults = [
+                'whitelist_network_only' => false,
+                'allow_local_override' => true
+            ];
+
+            foreach ($network_defaults as $option => $value) {
+                add_site_option($option, $value);
+            }
+        }
+
+        // Erstelle notwendige Datenbankeinträge für jede Rolle
+        $roles = ['subscriber', 'author', 'administrator'];
+        foreach ($roles as $role) {
+            $options = [
+                "whitelist_{$role}_entries" => '',
+                "whitelist_{$role}_enabled" => false
+            ];
+
+            if (is_multisite()) {
+                foreach ($options as $option => $value) {
+                    add_site_option($option, $value);
+                }
+            }
+            
+            foreach ($options as $option => $value) {
+                add_option($option, $value);
+            }
+        }
+
+        flush_rewrite_rules();
+    }
+
+    public static function deactivate() {
+        // Optional: Aufräumen bei Deaktivierung
+        // Kommentieren Sie die folgenden Zeilen ein, wenn die Einstellungen 
+        // bei Deaktivierung gelöscht werden sollen
+
+        /*
+        if (is_multisite()) {
+            delete_site_option('whitelist_network_only');
+            delete_site_option('allow_local_override');
+            
+            $roles = ['subscriber', 'author', 'administrator'];
+            foreach ($roles as $role) {
+                delete_site_option("whitelist_{$role}_entries");
+                delete_site_option("whitelist_{$role}_enabled");
+            }
+        }
+
+        $roles = ['subscriber', 'author', 'administrator'];
+        foreach ($roles as $role) {
+            delete_option("whitelist_{$role}_entries");
+            delete_option("whitelist_{$role}_enabled");
+        }
+        */
+
+        flush_rewrite_rules();
+    }
+}
